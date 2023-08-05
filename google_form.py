c3d990807bc8b18222671203ed7c024adc21426a
francolautaro2 <francooro322@gmail.com>
2023-08-05 18:30:22 -0300

    wip

--- google_form.py
+++ google_form.py
@@ -9,6 +9,7 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 
+from questions_form import preguntas, form
 
 SCOPES = ["https://www.googleapis.com/auth/forms.body"]
 
@@ -36,146 +37,6 @@ def create_form_service():
 
 
 # BatchUpdate para agregar o actualizar contenido del form
-
-preguntas = [
-    {
-        "questionItem": {
-            "question": {
-                "choiceQuestion": {
-                    "options": [
-                        {
-                            "value": "CRITICO"
-                        },
-                        {
-                            "value": "ALTO"
-                        },
-                        {
-                            "value": "MEDIO"
-                        },
-                        {
-                            "value": "BAJO"
-                        }
-                    ],
-                    "type": "RADIO"
-                },
-            }
-        },
-        "title": "¿En qué medida este archivo es crucial para las operaciones o actividades del negocio?"
-    },
-    {
-        "questionItem": {
-            "question": {
-                "choiceQuestion": {
-                    "options": [
-                        {
-                            "value": "SI"
-                        },
-                        {
-                            "value": "NO"
-                        }
-                    ],
-                    "type": "RADIO"
-                },
-            }
-        },
-        "title": "¿El archivo contiene información confidencial, privada o sensible?"
-    },
-    {
-        "questionItem": {
-            "question": {
-                "choiceQuestion": {
-                    "options": [
-                        {
-                            "value": "CRITICO"
-                        },
-                        {
-                            "value": "ALTO"
-                        },
-                        {
-                            "value": "MEDIO"
-                        },
-                        {
-                            "value": "BAJO"
-                        }
-                    ],
-                    "type": "RADIO"
-                },
-            }
-        },
-        "title": "Si el archivo se pierde o se filtra, ¿cuál sería el impacto en términos de confidencialidad, integridad y disponibilidad de la información?"
-    },
-    {
-        "questionItem": {
-            "question": {
-                "choiceQuestion": {
-                    "options": [
-                        {
-                            "value": "ALTO"
-                        },
-                        {
-                            "value": "MEDIO"
-                        },
-                        {
-                            "value": "BAJO"
-                        }
-                    ],
-                    "type": "RADIO"
-                },
-            }
-        },
-        "title": "¿Con qué frecuencia se necesita acceder a este archivo? ¿Es necesario acceder rápidamente en situaciones críticas?"
-    },
-    {
-        "questionItem": {
-            "question": {
-                "choiceQuestion": {
-                    "options": [
-                        {
-                            "value": "SI"
-                        },
-                        {
-                            "value": "NO"
-                        }
-                    ],
-                    "type": "RADIO"
-                },
-            }
-        },
-        "title": "¿El archivo está sujeto a regulaciones o requisitos de cumplimiento?"
-    },
-    {
-        "questionItem": {
-            "question": {
-                "choiceQuestion": {
-                    "options": [
-                        {
-                            "value": "SI"
-                        },
-                        {
-                            "value": "NO"
-                        }
-                    ],
-                    "type": "RADIO"
-                },
-            }
-        },
-        "title": "¿Otros procesos o sistemas dependen de este archivo para funcionar correctamente?"
-    },
-]
-
-# Plantilla del JSON
-form = {
-    "formId": "1G0o0I6xZyu3Etn36UgKcwgwqTfz3ja6mGlIbYkbZres",
-    "info": {
-        "documentTitle": "Formulario sin título",
-        "title": "Criticidad de Archivos"
-    },
-    "items": [
-       
-    ],
-    "responderUri": "https://docs.google.com/forms/d/e/1FAIpQLSdrW5P6cPXl8WNJULEzE0_bCmld0pwWxOY65ijtDcNc45JTWw/viewform"
-}
-
 
 # Obtiene los nombres y la criticidad de los archivos en la base de datos
 def get_names_db():
@@ -207,3 +68,6 @@ names = get_names_db()
 form = create_form(names)
 
 print(json.dumps(form, indent=4))
+
+def create_form_api():
+    pass
